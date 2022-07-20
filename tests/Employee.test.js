318b3ae8356f76e34865f83f0191b1ae0a294d57
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-20 16:07:08 -0700

    added jest

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -5,17 +5,33 @@ describe('Employee', () => {
         const employee = new Employee("Chelsea", 28, "chelseaburnham0@gmail.com", "Employee");
 
         expect(employee).toEqual({name: "Chelsea", age: 28, email: "chelseaburnham0@gmail.com", role: "Employee"});
-    });
-  
-    it('', () => {
-      
-    });
-  
-    it('', () => {
-      
-    });
-  
-    it('', () => {
-      
-    });
   });
+})
+
+describe('getName Method', () => {
+    it('should return the name of the user', () => {
+
+        expect(this.name).toEqual("Chelsea")
+  });
+})
+
+describe('getId Method', () => {
+    it('should return the id of the user', () => {
+
+        expect(this.id).toEqual(28)
+  });
+})
+
+describe('getEmail Method', () => {
+    it('should return the email of the user', () => {
+
+        expect(this.email).toEqual("chelseaburnham0@gmail.com")
+  });
+})
+
+describe('getName Method', () => {
+    it('should return the role of the user', () => {
+
+        expect(this.role).toEqual("Employee")
+  });
+})
