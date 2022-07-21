fe30aec3dc0c7d77777cb0f3a4813e4454267f9d
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-20 17:48:48 -0700

    added html layout

--- generateHtml.js
+++ generateHtml.js
@@ -1,7 +1,34 @@
 // generates HTML info 
   function generateHtml(response) {
-    return ``
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header class="jumbotron jumbotron-fluid bg-secondary text-white">
+            <div class="container">
+              <h1 class="display-4 text-center">My Team</h1>
+            </div>
+        </header>
+        <main class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${response.name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${response.role}</h6>
+              <p class="card-text">ID: ${response.id}</p>
+              <p class="card-text">School: ${response.school}</p>
+              <p class="card-text">Office Number: ${response.office}</p>
+              <a href="${response.email}" class="card-link" target="_blank">Email</a>
+              <a href="${response.github}" class="card-link" target="_blank">GitHub</a>
+            </div>
+        </main>
+    </body>
+    </html>`
   }
   
-  module.exports = generateMarkdown;
+  module.exports = generateHtml;
   
--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const Engineer = require("./lib/Engineer")
 const Manager = require("./lib/Manager")
 const Intern = require("./lib/Intern")
 
-// Function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("Success!")
-)}
-
 inquirer
     .prompt([
         {
@@ -32,12 +26,34 @@ inquirer
         {
             type: "list",
             message: "Please select the option that applies.",
-            name: "employee",
+            name: "role",
             choices: ["Employee", "Engineer", "Intern", "Manager"]
+        },
+        {
+            type: "input",
+            message: "What is your github link?",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the name of your school?",
+            name: "school"
+        },
+        {
+            type: "input",
+            message: "What is your office number?",
+            name: "office"
         },
     ])
     .then((response) => {
         console.log(response)
-         writeToFile("index.html", generateHtml(response));
+        fs.writeFile("index.html", generateHtml(response), (err) =>
+        err ? console.error(err) : console.log("Success!")
+        )
       }
     )
